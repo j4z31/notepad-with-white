3addcb2fabd31a004742f07ea2870711afe9b548
Jhasmany Quiroz <JhasmanyQuiroz@jalasoft.com>
2016-02-16 14:42:27 -0400

    The Notepad project with the save a file test

---
+++ NotepadSolution/Tests/Constant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tests
+{
+    public class Constant
+    {
+        public const string FOLDER_PATH = @"C:\Temp\";
+        public const string NOTEPAD_PATH = @"C:\Windows\System32\notepad.exe";
+        public const string TITLE_DOCUMENT = "Untitled - Notepad";
+        public const string TEXT = "This is a Test!!!";
+        public const string MENU_FILE = "File";
+        public const string SUBMENU_SAVE_AS = "Save As...";
+        public const string FILENAME = "FirstTest.txt";
+        public const string MESSAGE_FILE = "The File exists";
+        public const string ID_TXT_TEXT = "15";
+        public const string BTN_SEARCH_LOCATION = "Item 202";
+        public const string ID_FILENAME_SEARCH = "1001";
+        public const string CLASSNAME_FILENAME_SEARCH = "Edit";
+        public const string BTN_SAVE = "Save";
+    }
+}
---
+++ NotepadSolution/Tests/SaveFile.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using TestStack.White;
+using TestStack.White.InputDevices;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.WindowsAPI;
+
+namespace Tests
+{
+    [TestFixture]
+    public class SaveFile
+    {
+        private string folderPath;
+        private Application notepadApplication;
+        private Window notepadWindow;
+
+        [SetUp]
+        public void initialize()
+        {
+            folderPath = Constant.FOLDER_PATH;
+            Directory.CreateDirectory(folderPath);
+
+            notepadApplication = Application.Launch(Constant.NOTEPAD_PATH);
+            notepadWindow = notepadApplication.GetWindow(Constant.TITLE_DOCUMENT);
+        }
+
+        [Test]
+        public void saveTheFile()
+        {
+            SearchCriteria txtEditSearch = SearchCriteria.ByAutomationId(Constant.ID_TXT_TEXT);
+            var txtEdit = notepadWindow.Get(txtEditSearch);
+            Keyboard.Instance.Send(Constant.TEXT, txtEdit);
+
+            notepadWindow.MenuBar.MenuItem(Constant.MENU_FILE, Constant.SUBMENU_SAVE_AS).Click();
+
+            //Click on the address bar
+            SearchCriteria btnSearchLocation = SearchCriteria.ByAutomationId(Constant.BTN_SEARCH_LOCATION);
+            var txtToolbarAddress = notepadWindow.Get(btnSearchLocation);
+            txtToolbarAddress.Click();
+            Keyboard.Instance.Send(folderPath, txtToolbarAddress);
+
+            //Enter to path
+            notepadWindow.Keyboard.PressSpecialKey(KeyboardInput.SpecialKeys.RETURN);
+
+            // Click on the file name
+            SearchCriteria fileNameSearch = SearchCriteria.ByAutomationId(Constant.ID_FILENAME_SEARCH).AndByClassName(Constant.CLASSNAME_FILENAME_SEARCH);
+            var txtFileName = notepadWindow.Get(fileNameSearch);
+            txtFileName.DoubleClick();
+            notepadWindow.Keyboard.PressSpecialKey(KeyboardInput.SpecialKeys.BACKSPACE);
+            Keyboard.Instance.Send(Constant.FILENAME, txtFileName);
+
+            notepadWindow.Get(SearchCriteria.ByText(Constant.BTN_SAVE)).Click();
+
+            Assert.True((File.Exists(folderPath + Constant.FILENAME) ? true : false), Constant.MESSAGE_FILE);
+        }
+
+        [TearDown]
+        public void clean()
+        {
+            //notepadApplication.Kill();
+            notepadApplication.Close();
+            Directory.Delete(folderPath, true);
+        }
+    }
+}
